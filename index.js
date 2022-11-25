d05c708341090e683734aaa1441d4da022e41e2f
Dipra Das <dipradas5940@gmail.com>
2022-11-25 12:52:14 +0600

    User Registration

--- index.js
+++ index.js
@@ -5,16 +5,32 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.slxro.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const usersCollection = client.db('guitarBd').collection('users');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log());
 
 
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Server Running')
 })
 
